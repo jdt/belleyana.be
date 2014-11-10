779925790eb1886333ec8ac3a113e40161c41aaf
Jesper De Temmerman <jesper@toron.be>
2014-11-10 21:18:55 +0100

    Eerste versie van de layout zitten uitwerken

---
+++ Source/index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>BelleYana</title>
+
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
+       
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+        <style type="text/css">
+            body
+            {
+                background-color: #F4BF8E;
+
+                background-image: url("assets/img/background.png");
+                background-repeat:no-repeat;
+                background-position: top center;
+
+                font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;
+                font-size: 16px;
+            }
+
+            /* Customize container */
+            @media (min-width: 768px) {
+              .container {
+                max-width: 730px;
+              }
+            }
+
+            .container
+            {
+                background-color: #FFFFFF;
+                padding: 0;
+                margin-top: 15px;
+            }
+
+            .header
+            {
+                background-color: #C61E3D;
+            }
+
+            .header a
+            {
+                color: #FFFFFF;
+            }
+
+            .nav > li > a:hover,
+            .nav > li > a:focus {
+                text-decoration: none;
+                background-color: #9E0E28;
+                border-radius: 0 !important;
+            }
+
+            .nav-pills > li.active > a,
+            .nav-pills > li.active > a:hover
+            {
+                background-color: #56000F;
+                border-radius: 0 !important;
+            }
+
+
+            .content
+            {
+                padding: 10px 10px 0px 10px;
+            }
+
+            .footer
+            {
+                font-size: smaller;
+            }
+
+            h1
+            {
+                color: #C61E3D;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="banner">
+                <img src="assets/img/header.png" title="BelleYana.be" />
+            </div>
+            <div class="header">
+                <ul class="nav nav-pills nav-justified" role="tablist">
+                    <li role="presentation" class="active"><a href="#">Welkom</a></li>
+                    <li role="presentation"><a href="#">Producten</a></li>
+                    <li role="presentation"><a href="#">Behandelingen</a></li>
+                    <li role="presentation"><a href="#">Prijslijst</a></li>
+                    <li role="presentation"><a href="#">Contact</a></li>
+                </ul>
+            </div>
+            <div class="content">
+                <h1>Welkom</h1>
+                <p>
+                Schoonheidssalon Belle”Y”ana ontvangt u in een oase van rust, waar met passie en 
+                zorg uw moment van ontspanning gerealiseerd wordt. Gun uzelf de gelegenheid om 
+                t eervaren wat de Belle “Y” ana behandelingen voor u kunnen betekenen.staat voor 
+                uitzonderlijke kwaliteit. Wij bieden u een persoonlijke aanpak en behandelen elke 
+                vraag en/of probleem met de uiterste zorg. Schoonheidssalon Belle”Y”ana maak enkel 
+                gebruik van kwalitatief hoogstaande producten. Wij werken met het professioneel 
+                merk namelijk JEAN MARIN.
+                </p>
+                <p>
+                Ik verwelkom u graag in mijn sfeervolle salon.
+                </p>
+                <p>
+                Een ontspannen groet
+                </p>
+                <p>
+                Adam Yana, Belle “Y” ana
+                </p>
+            </div>
+
+            <div class="footer content">
+                <p>&copy; BelleYana | Volg ons op Facebook</p>
+            </div>
+        </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    </body>
+</html>
